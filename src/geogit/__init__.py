7a32cae6233cf0444742f6d769e6cc93ebaff121
Nils Adermann <naderman@naderman.de>
2010-12-12 15:11:05 +0100

    Added a class for an ipc fifo with specialised r/w access methods

--- src/geogit/__init__.py
+++ src/geogit/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from subprocess import Popen, STDOUT, PIPE
 from shlex import split
 import tempfile
@@ -15,10 +16,49 @@ def system(cmd):
         if read == '' and process.poll() != None:
             return value
 
+class GeoGitFifo(object):
+    def __init__(self, path):
+        self.path = path
+        self.reader = None
+        self.writer = None
+
+        try:
+            os.mkfifo(path)
+        except OSError, e:
+            # already created
+            pass
+
+    def read(self):
+        if self.reader is None:
+            self.reader = open(self.path, "r")
+
+        values = self.reader.readline().split(" ", 1)
+
+        return values
+
+    def write(self, rev, path):
+        if self.writer is None:
+            self.writer = open(self.path, "w")
+
+        print >> self.writer, rev + " " + path
+
+    def __del__(self):
+        if not self.reader is None:
+            self.reader.close()
+        if not self.writer is None:
+            self.writer.close()
+
 class GeoGit(object):
 
     def __init__(self):
         pass
+
+    def writeToFifo(self):
+        rev = system("git rev-parse HEAD").strip()
+        path = system("git rev-parse --show-toplevel").strip()
+
+        fifo = GeoGitFifo(os.environ["HOME"] + "/.geogit.sock")
+        fifo.write(rev, path)
 
     def attach_note(self):
         note_file = tempfile.NamedTemporaryFile()
@@ -37,7 +77,8 @@ def main(argv=None):
         argv = sys.argv
 
     geogit = GeoGit()
-    geogit.attach_note()
+    geogit.writeToFifo()
+    #geogit.attach_note()
 
 if __name__ == "__main__":
     sys.exit(main())
