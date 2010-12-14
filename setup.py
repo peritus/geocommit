59dc30b5a853ad970dfe0dba5f0196878df25014
Filip Noetzel <filip@j03.de>
2010-12-14 18:25:55 +0100

    Use logo as icon for GeoGit.app

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from os.path import curdir, abspath, join
 from distutils.util import get_platform
 from setuptools import setup
 
@@ -9,7 +10,10 @@ setup_requires = []
 
 APP = ['src/geogit/corelocation.py']
 DATA_FILES = []
-OPTIONS = {'argv_emulation': False}
+OPTIONS = {
+    'argv_emulation': False,
+    'iconfile': join(abspath(curdir), 'src/geogit/geogit_logo.icns'),
+}
 
 if get_platform().startswith('macosx'):
     install_requires += ['pyobjc-core', 'pyobjc-framework-Cocoa', 'pyobjc-framework-CoreLocation']
