fb78b8ad4059bc3559a9b7d65ceac3052ac87e68
Filip Noetzel <filip@j03.de>
2010-12-18 20:17:49 +0100

    Monkey-patch distutils to build an executable (instead of a .so)

--- setup.py
+++ setup.py
@@ -5,13 +5,36 @@ from os.path import curdir, abspath, join
 from distutils.util import get_platform
 from setuptools import setup
 from setuptools.extension import Extension
+from distutils.command.build_ext import build_ext as _build_ext
 
-ext_modules = []
+extra_args = {}
 
 if get_platform().startswith('macosx'):
-    ext_modules += [
+    class build_ext(_build_ext):
+        '''
+        This thing is an evil monkey-patch HACK:
+
+        Instead of building a shared object file (.so) out of the
+        objective-c source code provided, it builds an executable.
+
+        Communication with that executable is then via stdout
+        '''
+        def build_extension(self, ext):
+            def _link_executable(objects, ext_filename, **kwargs):
+                del kwargs['export_symbols']
+                del kwargs['build_temp']
+                ext_filename = ext_filename.rstrip('.so')
+
+                self.compiler.link_executable(objects, ext_filename, **kwargs)
+
+            self.compiler.link_shared_object = _link_executable
+            _build_ext.build_extension(self, ext)
+
+    extra_args['cmdclass'] = { 'build_ext': build_ext }
+
+    extra_args['ext_modules'] = [
         Extension(
-          name='_geocommit_corelocationprovider',
+          name = 'geogit.corelocationprovider',
           sources=['src/geogit/corelocationprovider/geocommit.m', 'src/geogit/corelocationprovider/geocommitdelegate.m'],
           extra_link_args=['-framework', 'Foundation', '-framework', 'CoreLocation']
         ),
@@ -19,7 +42,6 @@ if get_platform().startswith('macosx'):
 
 setup(
     name="geogit",
-    ext_modules=ext_modules,
     version='0.1dev0',
     description="Geolocation for git",
     author="Nils Adermann <naderman+geogit@naderman.de>, Filip Noetzel <filip+geogit@j03.de>",
@@ -30,4 +52,5 @@ setup(
     packages=['geogit'],
     package_dir={'': 'src'},
     entry_points = {'console_scripts': ['geogit = geogit:main']},
+    **extra_args
     )
