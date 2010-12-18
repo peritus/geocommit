887bef8486be18f9365c5d307d60d1c55719c0be
Filip Noetzel <filip@j03.de>
2010-12-18 20:15:49 +0100

    We no longer do the pyobj-c thing

--- setup.py
+++ setup.py
@@ -6,20 +6,9 @@ from distutils.util import get_platform
 from setuptools import setup
 from setuptools.extension import Extension
 
-install_requires = []
-setup_requires = []
 ext_modules = []
 
-APP = ['src/geogit/corelocation.py']
-DATA_FILES = []
-OPTIONS = {
-    'argv_emulation': False,
-    'iconfile': join(abspath(curdir), 'src/geogit/geogit_logo.icns'),
-}
-
 if get_platform().startswith('macosx'):
-    install_requires += ['pyobjc-core', 'pyobjc-framework-Cocoa', 'pyobjc-framework-CoreLocation']
-    setup_requires += ['py2app']
     ext_modules += [
         Extension(
           name='_geocommit_corelocationprovider',
@@ -30,10 +19,7 @@ if get_platform().startswith('macosx'):
 
 setup(
     name="geogit",
-    app=APP,
     ext_modules=ext_modules,
-    data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
     version='0.1dev0',
     description="Geolocation for git",
     author="Nils Adermann <naderman+geogit@naderman.de>, Filip Noetzel <filip+geogit@j03.de>",
@@ -44,6 +30,4 @@ setup(
     packages=['geogit'],
     package_dir={'': 'src'},
     entry_points = {'console_scripts': ['geogit = geogit:main']},
-    install_requires=install_requires,
-    setup_requires=setup_requires,
     )
