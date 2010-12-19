45cf8f768166c591d63140292fd0119150a6e4fe
Filip Noetzel <filip@j03.de>
2010-12-19 01:51:42 +0100

    CoreLocationProvider.parse_funky_description_dump

--- src/geocommit/provider/corelocation/__init__.py
+++ src/geocommit/provider/corelocation/__init__.py
@@ -12,10 +12,51 @@ if exists(EXECUTABLE):
     ''' I'm on a mac! '''
 
     class CoreLocationProvider(LocationProvider):
+
+        @classmethod
         def parse_funky_description_dump(self, string):
-            lat = 52.0
-            long = 6.0
-            return Location(lat, long, src='CoreLocation')
+            '''
+            Output of the objective-c code looks like this:
+
+            >>> line1 = "2010-12-19 01:25:49.101 geocommit[42427:903] <+53.46278416, +6.32637026> +/- 151.00m (speed -1.00 mps / course -1.00) @ 2010-12-19 01:24:41 +0100\\n"
+            >>> line2 = "2010-12-19 01:25:50.244 geocommit[42427:903] <+53.46278817, +12.32638062> +/- 149.00m (speed 0.00 mps / course -1.00) @ 2010-12-19 01:25:49 +0100\\n"
+            >>> CoreLocationProvider.parse_funky_description_dump(line1 + line2)
+            <Location(geocommit(1.0): lat 53.46278817, long 12.32638062, src CoreLocation;)>
+
+            This function will always parse the latter location
+
+            >>> CoreLocationProvider.parse_funky_description_dump(line2 + line1)
+            <Location(geocommit(1.0): lat 53.46278416, long 6.32637026, src CoreLocation;)>
+            '''
+
+            for line in string.split('\n'):
+                tokens = line.split(' ')
+
+                if len(tokens) < 16:
+                    # newline at end of output
+                    continue
+
+                # [0] '2010-12-19',
+                # [1] '01:25:49.101',
+                # [2] 'geocommit[42427:903]',
+                # [3] '<+53.46278416,',
+                lat = tokens[3].lstrip('<+').rstrip(',')
+                # [4] '+12.32637026>',
+                lng = tokens[4].lstrip('+').rstrip('>')
+                # [5] '+/-',
+                # [6] '151.00m',
+                # [7] '(speed',
+                # [8] '-1.00',
+                # [9] 'mps',
+                # [10] '/',
+                # [11] 'course',
+                # [12] '-1.00)',
+                # [13] '@',
+                # [14] '2010-12-19',
+                # [15] '01:24:41',
+                # [16] '+0100',
+
+            return Location(lat, lng, src='CoreLocation')
 
         def get_location(self):
             parseme = system(EXECUTABLE)
--- src/geocommit/tests.py
+++ src/geocommit/tests.py
@@ -4,6 +4,7 @@ import unittest
 def test_suite():
     return unittest.TestSuite([
       doctest.DocTestSuite('geocommit'),
+      doctest.DocTestSuite('geocommit.provider.corelocation'),
       doctest.DocTestSuite('geocommit.locationprovider'),
       doctest.DocTestSuite('geocommit.networkmanager'),
       doctest.DocTestSuite('geocommit.wifilocationprovider'),
