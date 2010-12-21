9a6cd0bfb07f23ff7f97dad558f1d3c92f774cea
David Soria Parra <dsp@php.net>
2010-12-21 01:30:26 +0100

    Github/Bitbucket autodetection
    Add a bitbucket/github autodetection based on the payload we get.

--- website/geocommit/src/geocommit/hook.clj
+++ website/geocommit/src/geocommit/hook.clj
@@ -11,27 +11,43 @@
 	clojure.contrib.json
 	[ring.util.servlet :only [defservice]])
   (:require [compojure.route :as route]
-	    [appengine-magic.core :as ae]))
+	    [appengine-magic.core :as ae]
+	    [clojure.contrib.trace :as t]))
+(def *couchdb* (get-config :database :geocommits))
+
+(defn- contains-all? [val & keys]
+  (every? #(= true %)
+	  (map #(clojure.core/contains? val %) keys)))
 
 (defn is-tracked? [repo] true)
 
-(def *couchdb* (get-config :database :geocommits))
-
-(defn parse-bitbucket-update [url commits]
+(defn- bitbucket-update-parser [url commits]
   (let [ctx (remove nil? (map #(parse-geocommit url (%1 :node) (%1 :author) (%1 :message)) commits))]
     (if (empty? ctx)
       nil ctx)))
 
+(defn- guess-origin [payload]
+  (cond
+   (contains-all? payload :broker :service) :bitbucket
+   (contains-all? payload :before :ref) :github))
+
+(defn- bitbucket [url commits]
+  (if (is-tracked? url)
+    (if-let [ctx (bitbucket-update-parser url commits)]
+      (if-let [res (couch-bulk-update *couchdb* ctx)]
+	{:status 201}
+	{:status 400})
+      {:status 200})
+    {:status 200}))
+
+(defn- github [url commits]
+  {:status 400})
 
 (defn app-hook [payload]
-  (let [json (read-json payload)
-	commits (json :commits)
-	repo (json :repository)
-	url (str "http://bitbucket.org" (repo :absolute_url))]
-    (if (and (is-tracked? url) (vector? commits))
-      (if-let [ctx (parse-bitbucket-update url commits)]
-	(if-let [res (couch-bulk-update *couchdb* ctx)]
-	  {:status 201}
-	  {:status 400})
-	{:status 200})
-      {:status 200})))
+  (let [json (read-json payload)]
+    (condp = (guess-origin json)
+	:github (github (json :url)
+			(json :commits))
+	:bitbucket (bitbucket (str "http://bitbucket.org/"
+				   (json :repository))
+			      (json :commits)))))
