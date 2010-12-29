2f9e0bf496cacb3c43df016efe3bd2b30ac46bf2
Nils Adermann <naderman@naderman.de>
2010-12-29 23:34:43 +0100

    Attempt to build war and jar files with sbt

--- fetchservice/project/build/FetchServiceProject.scala
+++ fetchservice/project/build/FetchServiceProject.scala
@@ -1,6 +1,11 @@
 import sbt._
 
-class FetchServiceProject(info: ProjectInfo) extends DefaultWebProject(info) {
+class FetchServiceProject(info: ProjectInfo) extends ParentProject(info) {
+    override def shouldCheckOutputDirectories = false
+
+    lazy val web  = project(".", "Web Service", new WebServiceProject(info))
+    lazy val worker = project(".",  "Worker", new ScanInitWorkerProject(info))
+
     val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
     val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.v20100331" % "test"
@@ -8,4 +13,12 @@ class FetchServiceProject(info: ProjectInfo) extends DefaultWebProject(info) {
     val beanstalk = "com.surftools" % "BeanstalkClient" % "1.4.4-SNAPSHOT"
     val liftjson = "net.liftweb" % "lift-json_2.8.0" % "2.1"
     val scouchdb = "scouch.db" % "scouchdb_2.8.0" % "0.6"
+
+    class WebServiceProject(info: ProjectInfo) extends DefaultWebProject(info) {
+    }
+
+    class ScanInitWorkerProject(info: ProjectInfo) extends DefaultProject(info) {
+        override def mainClass = Some("ScanInitWorker")
+    }
+
 }
