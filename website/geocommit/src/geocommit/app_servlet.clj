c14e14e180f8e58243cafb2e77e5fc06042a12ce
David Soria Parra <dsp@php.net>
2011-04-15 00:24:31 +0200

    Handle payload based on on the content-type
    Our github service sends plain json in the body.
    If the content-type indicates githubpostreceive
    we read the payload from the body.

--- website/geocommit/src/geocommit/app_servlet.clj
+++ website/geocommit/src/geocommit/app_servlet.clj
@@ -11,18 +11,22 @@
   (:use compojure.core
 	[geocommit.hook :only [app-hook]]
 	[geocommit.api :only [app-api-geocommits]]
-	[geocommit.signup :only [app-verify-hook app-signup]])
+	[geocommit.signup :only [app-verify-hook app-signup]]
+	[geocommit.http :only [mslurp]])
   (:use [appengine-magic.servlet :only [make-servlet-service-method]]
 	[appengine-magic.core :as ae])
-  (:require [compojure.route :as route]))
+  (:require [compojure.route :as route]
+	    [clojure.contrib.trace :as t]))
 
 (defroutes handler
   (GET "/api/geocommits" [payload] (app-api-geocommits payload))
-  (POST "/api*" [payload] (app-hook payload))
+  (POST "/api*" payload
+	(if (= (:content-type payload) "application/githubpostreceive+json")
+	  (app-hook (mslurp (.getInputStream (:request payload))))
+	  (app-hook (:payload (:params payload)))))
   (GET "/signup/verify/:code" [code] (app-verify-hook code))
   (POST "/signup*" [mailaddr] (app-signup mailaddr))
   (route/not-found "not a valid request"))
-
 (ae/def-appengine-app geocommit-application #'handler)
   
 (defn -service [this request response]
