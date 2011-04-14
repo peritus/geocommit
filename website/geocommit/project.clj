6df592492c0c85262077f4fcaf1feed7ba6b10b3
David Soria Parra <dsp@php.net>
2011-04-14 23:30:18 +0200

    Update project.clj

--- website/geocommit/project.clj
+++ website/geocommit/project.clj
@@ -3,17 +3,16 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.5.3"]
-                 [clj-http "0.1.2-ae"]
+                 [clj-http-ae "0.1.2"]
                  [commons-validator/commons-validator "1.3.1"]
                  [oro/oro "2.0.8"]
                  [ring/ring "0.3.5"]]
-  :dev-dependencies [[autodoc "0.7.1"]
-                     [swank-clojure "1.2.0"]
-                     [appengine-magic "0.3.1"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
+                     [appengine-magic "0.4.1"]]
   :aot [geocommit.app_servlet geocommit.services]
-  :repositories {"java.net" "http://download.java.net/maven/2"}
-  :autodoc {:name "geocommit",
-      :page-title "geocommit.com HTTP API",
-      :copyright "Copyright 2010 The Geocommit Project, David Soria Parra"
-      :description "The geocommit.com HTTP API is a simple rest API
-to store/retrieve geocommits in/from the geocommit.com database"})
+  :repositories {"java.net" "http://download.java.net/maven/2"})
+;  :autodoc {:name "geocommit",
+;      :page-title "geocommit.com HTTP API",
+;      :copyright "Copyright 2010 The Geocommit Project, David Soria Parra"
+;      :description "The geocommit.com HTTP API is a simple rest API
+;to store/retrieve geocommits in/from the geocommit.com database"})
