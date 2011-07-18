83ef3ac50da6bd1074e2690c7f3b117f3328fccb
Filip Noetzel <filip@j03.de>
2011-07-18 21:40:01 +0200

    v0.9.3beta1

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ if get_platform().startswith('macosx'):
 
 setup(
     name="geocommit",
-    version='0.9.2',
+    version='0.9.3beta1',
     description="Geolocation for git",
     long_description=open('README.rst').read(),
     author="Nils Adermann <naderman+geocommit@naderman.de>, Filip Noetzel <filip+geocommit@j03.de>, David Soria Parra <dsp+geocommit@experimentalworks.net>",
